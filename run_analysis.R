7117a6b37a54a5cffcbb36069ba5358361555018
AnastasiaMorozova <32030107+AnastasiaMorozova@users.noreply.github.com>
2017-11-12 21:29:00 +0100

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,1 +1,9 @@
-# data set
+# This script creates a tidy data set out of raw training and test data sets
+# from the accelerometers from the Samsung Galaxy S smartphone.
+
+#  Each part of the script starts with a comment line describing the procedures 
+# applied to prepare a tidy data set.
+
+# Read data from training and test sets
+trainingSet<-read.table("X_train.txt")  # read text file "X_train.txt"
+testSet<-read.table("X_test.txt")       # read text file "X_test.txt"
