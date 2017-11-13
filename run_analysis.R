2678eeb6b7e8a1f04595d8894f24054610335eed
AnastasiaMorozova <32030107+AnastasiaMorozova@users.noreply.github.com>
2017-11-13 14:48:22 +0100

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -4,6 +4,28 @@
 #  Each part of the script starts with a comment line describing the procedures 
 # applied to prepare a tidy data set.
 
+# Attach necessary libraries
+library(stringr)
+library(reshape2)
+library(plyr)
+
 # Read data from training and test sets
 trainingSet<-read.table("X_train.txt")  # read text file "X_train.txt"
 testSet<-read.table("X_test.txt")       # read text file "X_test.txt"
+# Check the size and type of data in the data sets
+str(trainingSet)
+str(testSet)
+
+
+# Creating a merged data set by binding training and test data sets
+mergedDataSet<-rbind(trainingSet,testSet)
+
+# Assign the names of the features to the columns of the data set
+features<-read.table("features.txt")              # read the features 
+grep("mean",features$V2)
+
+subjectsTest<-read.table("subject_test.txt")      # subjects test 
+subjectTrain<-read.table("subject_train.txt")     # subjects train
+activityLabels<-read.table("activity_labels.txt") # activity labels
+ltest<-read.table("y_test.txt")   # activity labels for test set
+ltrain<-read.table("y_train.txt") # activity labels for train set
